7fa44e248bc6cc1481e205290f209433d5bfdb56
gabriel-cr <gabrielcristian.ionescu@gmail.com>
2016-04-09 20:10:53 +0300

    compile qemu and sister projects in one shot

--- configure_qsdk.sh
+++ configure_qsdk.sh
@@ -1,1 +1,27 @@
-make clean;./configure --target-list=arm-softmmu --enable-debug --disable-werror --extra-cflags="-rdynamic -fPIC"
+#!/bin/bash
+make clean
+
+qemu_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+if [[ ! -e device-repo ]]; then
+    echo device-repo not found - setting up
+    git clone https://github.com/gabriel-cr/device-repo.git
+fi
+
+cd device-repo
+./make_prj.sh
+cd $qemu_dir
+
+if [[ ! -e lib ]]; then
+    ln -s device-repo/lib lib
+fi
+
+if [[ ! -e pgback ]]; then
+    git clone https://github.com/gabriel-cr/pgback
+fi
+
+cd pgback
+./make_prj.sh
+cd $qemu_dir
+
+./configure --target-list=arm-softmmu --enable-debug --disable-werror --extra-cflags="-rdynamic -fPIC"
